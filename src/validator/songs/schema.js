b845d7d405f309e5c2a7b321e25554f75c29e418
airellrmdn <airellramadhan@gmail.com>
2025-03-02 15:42:05 +0700

    fix: data validation for users and albums

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
 
+const year = new Date().getFullYear();
+
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1900).max(year).required(),
   genre: Joi.string().required(),
   performer: Joi.string().required(),
   duration: Joi.number(),
